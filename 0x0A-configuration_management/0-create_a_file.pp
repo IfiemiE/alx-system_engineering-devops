62f762e0c4d52d67978c2204d58ca1bcf65fc4df
IfiemiE <erigiifiemi@gmail.com>
2023-03-24 21:44:48 +0000

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates a file: /tmp/school
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
